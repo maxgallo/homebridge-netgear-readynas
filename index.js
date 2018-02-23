3423b25058ceeeab626a151a9e1ea7a246ad6a86
Max Gallo <massimiliano.gallo@gmail.com>
2018-02-23 00:32:47 +0000

    moves myswitch inside

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 const shutdow = require('./commands/shutdown');
 const wakeup = require('./commands/wakeup');
 const status = require('./commands/status');
-const Service, Characteristic;
-
-function mySwitch(log, config) {
-    this.log = log;
-    //this.getUrl = url.parse(config['getUrl']);
-    //this.postUrl = url.parse(config['postUrl']);
-}
-
-mySwitch.prototype = {
-    getServices: function () {
-        let informationService = new Service.AccessoryInformation();
-        informationService
-            .setCharacteristic(Characteristic.Manufacturer, "Netgear")
-            .setCharacteristic(Characteristic.Model, "ReadyNAS")
-            .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
-
-        let switchService = new Service.Switch("ReadyNAS");
-        switchService
-            .getCharacteristic(Characteristic.On)
-            .on('get', this.getSwitchOnCharacteristic.bind(this))
-            .on('set', this.setSwitchOnCharacteristic.bind(this));
-
-        this.informationService = informationService;
-        this.switchService = switchService;
-        return [informationService, switchService];
-    },
-    getSwitchOnCharacteristic: function (next) {
-        const nasStatus = status();
-        next(null, nasStatus);
-    },
-    setSwitchOnCharacteristic: function (on, next) {
-        if (on) {
-            wakeup();
-            return next();
-        }
-
-        shutdow();
-        next();
-    }
-};
 
 function setup(homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
+    const Service = homebridge.hap.Service;
+    const Characteristic = homebridge.hap.Characteristic;
+
+    function mySwitch(log, config) {
+        this.log = log;
+        //this.getUrl = url.parse(config['getUrl']);
+        //this.postUrl = url.parse(config['postUrl']);
+    }
+
+    mySwitch.prototype = {
+        getServices: function () {
+            let informationService = new Service.AccessoryInformation();
+            informationService
+                .setCharacteristic(Characteristic.Manufacturer, "Netgear")
+                .setCharacteristic(Characteristic.Model, "ReadyNAS")
+                .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+
+            let switchService = new Service.Switch("ReadyNAS");
+            switchService
+                .getCharacteristic(Characteristic.On)
+                .on('get', this.getSwitchOnCharacteristic.bind(this))
+                .on('set', this.setSwitchOnCharacteristic.bind(this));
+
+            this.informationService = informationService;
+            this.switchService = switchService;
+            return [informationService, switchService];
+        },
+        getSwitchOnCharacteristic: function (next) {
+            const nasStatus = status();
+            next(null, nasStatus);
+        },
+        setSwitchOnCharacteristic: function (on, next) {
+            if (on) {
+                wakeup();
+                return next();
+            }
+
+            shutdow();
+            next();
+        }
+    };
+
     homebridge.registerAccessory("switch-plugin", "ReadyNasSwitch", mySwitch);
 }
 
